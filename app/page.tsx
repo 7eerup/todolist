b85dd6fb9b34d3306758ffe2cbbe003b3c3d06e0
7eerup <7eerup@gmail.com>
2025-03-13 12:53:11 +0900

    UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
+import UI from "./ui";
 
 export default function Home() {
   return <main>
-    Todo List
-    <i className="fas fa-check"></i>
+    <UI />
     </main>
 }
