ae2a21dbf99753a976b0e12178548c7e79539aa7
7eerup <7eerup@gmail.com>
2025-03-13 14:44:35 +0900

    CRUD

--- components/todo.tsx
+++ components/todo.tsx
@@ -1,35 +1,84 @@
-"use client"
+"use client";
 
-import { Checkbox, IconButton } from "@material-tailwind/react"
+import { Checkbox, IconButton, Spinner } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { deleteTodo, updateTodo } from "actions/todo-actions";
+import { queryClient } from "config/ReactQueryClientProvider";
 import { useState } from "react";
 
-export default function Todo({}) {
-    const [isEditing, setIsEditing] = useState(false);
-    const  [completed, setCompleted] = useState(false)
-    const [title, setTitle] = useState("")
+export default function Todo({ todo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [completed, setCompleted] = useState(todo.completed);
+  const [title, setTitle] = useState(todo.title);
 
-    return (
+  const updateTodoMutation = useMutation({
+    mutationFn: () =>
+      updateTodo({
+        id: todo.id,
+        title,
+        completed,
+      }),
+    onSuccess: () => {
+      setIsEditing(false);
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      });
+    },
+  });
+
+  const deleteTodoMutation = useMutation({
+    mutationFn: () => deleteTodo(todo.id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      });
+    },
+  });
+
+  return (
     <div className="w-full flex items-center gap-1">
-        <Checkbox checked={completed} onChange={(e) => setCompleted(e.target.checked)} />
+      <Checkbox
+        checked={completed}
+        onChange={async (e) => {
+          await setCompleted(e.target.checked);
+          await updateTodoMutation.mutate();
+        }}
+      />
 
-        {isEditing ? (
-            <input className="flex-1 border-b-black border-b pb-1" value={title} onChange={(e) => setTitle(e.target.value)} />
-        ) : (
+      {isEditing ? (
+        <input
+          className="flex-1 border-b-black border-b pb-1"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      ) : (
         <p className={`flex-1 ${completed && "line-through"}`}>{title}</p>
-        )}
+      )}
 
-        {isEditing ? (
-        <IconButton onClick={() => setIsEditing(false)}>
-        <i className="fas fa-check" />
+      {isEditing ? (
+        <IconButton
+          onClick={async () => {
+            await updateTodoMutation.mutate();
+          }}
+        >
+          {updateTodoMutation.isPending ? (
+            <Spinner />
+          ) : (
+            <i className="fas fa-check" />
+          )}
         </IconButton>
-        ) : (
+      ) : (
         <IconButton onClick={() => setIsEditing(true)}>
-            <i className="fas fa-pen" />
+          <i className="fas fa-pen" />
         </IconButton>
+      )}
+      <IconButton onClick={() => deleteTodoMutation.mutate()}>
+        {deleteTodoMutation.isPending ? (
+          <Spinner />
+        ) : (
+          <i className="fas fa-trash" />
         )}
-        <IconButton>
-            <i className="fas fa-trash" />
-        </IconButton>
-        </div>
-    );
+      </IconButton>
+    </div>
+  );
 }
